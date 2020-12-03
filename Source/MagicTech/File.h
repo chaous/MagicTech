96c7622efa8629938cd92edb5061a0fba25faa40
chaous <64496742+chaous@users.noreply.github.com>
2020-12-04 00:07:03 +0300

    Revert "Revert "сделал взлом как в fallout""
    This reverts commit 205d938e00c0858547de988c7a57a25984f18835.

---
+++ Source/MagicTech/File.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MAGICTECH_API File
+{
+public:
+	File();
+	~File();
+
+	FString Worlds;
+};

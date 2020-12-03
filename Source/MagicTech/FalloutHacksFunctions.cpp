0bc9b43b9d7df19cbe963b7f38ed14c1f58ce320
chaous <64496742+chaous@users.noreply.github.com>
2020-12-04 01:00:33 +0300

    Revert "Revert "сделал взлом как в fallout""
    This reverts commit 205d938e00c0858547de988c7a57a25984f18835.

---
+++ Source/MagicTech/FalloutHacksFunctions.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FalloutHacksFunctions.h"
+
+#include <cwctype>
+
+#include "Math/UnrealMathUtility.h"
+
+
+
+int UFalloutHacksFunctions::NumberOfWorlds(FString text)
+{
+	int numberOfWorlds = 0;
+	for(auto i : text)
+	{
+		if(i == '\n')
+			++numberOfWorlds;
+	}
+	return numberOfWorlds;
+}
+
+FString UFalloutHacksFunctions::GetRandomWorld(FString text)
+{
+	int RandomWorldNumber = FMath::RandRange(0, 93390);
+	int WorldNumber = 0;
+	FString world = TEXT("");
+	for (auto i : text)
+	{
+		if (i != '\n')
+		{
+			if (WorldNumber == RandomWorldNumber && std::iswalpha(i))
+				world += i;
+		}
+		else
+		{
+			++WorldNumber;
+		}
+	}
+	//world += '\n';
+	return world;
+}
+
+FString UFalloutHacksFunctions::ReturnEnter()
+{
+	return "\n";
+}
+
+int UFalloutHacksFunctions::NumberOfTheSameCharecters(FString a, FString b)
+{
+	int x = 0, y = 0;
+	for(auto i : a)
+	{
+		for(auto j : b)
+		{
+			if (i == j)
+			{
+				++x;
+				break;
+			}
+		}
+	}
+
+	for (auto i : b)
+	{
+		for (auto j : a)
+		{
+			if (i == j)
+			{
+				++y;
+				break;
+			}
+		}
+	}
+	return FMath::Min(x, y);
+	
+}
+
+
+
+
+
+
+
